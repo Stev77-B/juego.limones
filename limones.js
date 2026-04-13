be187221e581f717dbf59ab9a81c223bba4aab2b
BlackmanCub <pedroenriquerhrh@gmail.com>
2026-04-13 12:49:27 -0500

    bajar limon

--- limones.js
+++ limones.js
@@ -4,11 +4,17 @@ let ctx=canvas.getContext("2d");
 const ALTURA_SUELO=40;
 const ALTURA_PERSONAJE=60;
 const ANCHO_PERSONAJE=40;
+const ANCHO_LIMON=20;
+const ALTO_LIMON=20;
+
 let personajeX=canvas.width/2;
+let limonX=canvas.width/2
+let limonY=5;
 
 function iniciar(){
     dibujarSuelo();
     dibujarPersonaje();
+    dibujarLimon();
 }
 function dibujarSuelo(){
      ctx.fillStyle="blue";
@@ -23,14 +29,41 @@ function dibujarPersonaje(){
 function moverIzquierda(){
      personajeX=personajeX-10;
       actualizarPantalla();  
+}
+
+function moverDerecha(){
+    personajeX=personajeX+10;
+    actualizarPantalla();
+}
 
 function actualizarPantalla(){
   limpiarCanva();
      dibujarSuelo();
      dibujarPersonaje();
+     dibujarLimon();
 }
-}
+
 
 function limpiarCanva(){
     ctx.clearRect(0,0,canvas.width,canvas.height);
+}
+
+function dibujarLimon(){
+     ctx.fillStyle="green";
+     ctx.fillRect(limonX,limonY,ANCHO_LIMON,ALTO_LIMON)
+       
+
+    }
+
+
+function bajarLimon(){
+    limonY = limonY + 10;
+    
+    actualizarPantalla();
+}
+
+function dectectarColision(){
+    if(limonX==personajeX && limonY == personajeY){
+
+    }
 }
