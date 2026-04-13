f0d2c6837c5aae772c63a346d9959a03da771fab
BlackmanCub <pedroenriquerhrh@gmail.com>
2026-04-13 14:08:22 -0500

    Puntaje y vidas

--- utils.js
+++ utils.js
@@ -1,7 +1,18 @@
 function generarAleatorio(min,max){
-       let random=Math.random();
-    let numero=random*(max-min);
+    let random=Math.random();
+    let numero=random*(max-min+1);
     let numeroEntero = Math.ceil(numero);
-    numeroEntero = numeroEntero+min;
+    numeroEntero = numeroEntero+min-1;
      return numeroEntero
  }
+
+
+
+
+
+
+
+function mostrarEnSpan(idSpan,valor){
+      let componente=document.getElementById(idSpan);
+      componente.textContent =valor; 
+}
